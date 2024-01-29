a5df955f1d5d1fd4bb60cfc976f23f9a06641712
prjesus12 <75141984+prjesus12@users.noreply.github.com>
2024-01-29 06:21:45 -0400

    new changes

--- src/config/Router.php
+++ src/config/Router.php
@@ -1,44 +1,38 @@
 <?php
 namespace Config;
 
-class Router {
+class Router
+{
+
+    private $routes = [];
+
+    function add($path, $controller, $action) {
+        $this->routes[$path] = [
+            "_c" => $controller,
+            "_a" => $action
+        ];
+    }
+
+    private function validateController()
+    {
+        $route = Request::exists("route");
+        if(isset($this->routes[$route]) == false){
+            die("route not found");
+        }
+    }
     
-   
-
-    private function validateController() {
-        if(isset($_GET['_c']) == false){
-            die("Mising _c parameter");
-        }
-    }
-    private function validateAction() {
-        if(isset($_GET['_a']) == false){
-            die("Mising _a parameter");
-        }
-    }
-
-    private function verifyController($class){
-        if(class_exists("Controllers\\$class") == false){
-            die("Mising controller");
-        }
-    }
-    private function verifyMethod($class, $method){
-        if(method_exists("Controllers\\$class", $method) == false){
-            die("Mising method");
-        }
-    }
-  
-
-    function run() {
+    function run()
+    {
         $this->validateController();
-        $this->validateAction();
 
-        $this->verifyController($_GET["_c"]);
-        $this->verifyMethod($_GET["_c"], $_GET["_a"]);
+        $route = Request::exists("route");
 
-        $_c = "Controllers\\$_GET[_c]";
-        $action = $_GET['_a'];
+        $current = $this->routes[$route];
+
+        $_c = "Controllers\\$current[_c]";
+        $action = $current['_a'];
         $controller = new $_c;
-        
+
         return Response::parseResponse($controller->$action());
     }
 
